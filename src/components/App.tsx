4388e95860d3253317f38f105c1a9257bc2830c6
Amanda <amanherrer@gmail.com>
2021-10-21 21:01:10 -0600

    21/10

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,42 +1,29 @@
 import React from 'react';
-// import { Calculator } from './Calculator';
-import { Counter } from './Counter';
-// import { Container } from './Container';
-// import { HelloClass } from './HelloClass';
-import { Hello } from './Hello';
-import { Wrapper } from './Wrapper';
-
-export type User = {
-  name: string;
-  age: number;
-  email: string;
-  address: string;
-}
-
+import { Home } from './Home';
+import { Switch,Route } from 'react-router';
+import { About } from './About';
+import { MyTodoList } from './MyToDoList';
+import { NotFound } from './404';
+import { Nav } from './Nav';
 const App: React.FC = () => {
-  const users: User[] = [{ name: 'Salvador', age: 30, email: 'salvador@ucreativa.com', address: 'San Jose' }, { name: 'Amanda', age: 34, email: 'amanda@ucreativa.com', address: 'San Jose' }, { name: 'Lester', age: 20, email: 'lester@ucreativa.com', address: 'San Jose' }];
-  const filteredUsers = users.filter((user) => user.age < 30);
-  const totalAge = users.reduce((prevUser, currentUser) => prevUser + currentUser.age, 0);
-  const ageMessage = totalAge <= 20 ? 'Personas muy jovenes' : totalAge <= 60 ? 'Persona menores a 40 anos' : 'Personas';
-
-  const handleUser = (name: string) => console.log('Click en usuario', name);
-
-  return <React.Fragment>
-    <h2>Lista de Usuarios</h2>
-    {/* {users.map((user) => <Hello key={user.email} name={user.name} age={user.age} email={user.email}/>)} */}
-    <p>Edad total {totalAge} {ageMessage}</p>
-    <h3>Lista de Usuarios Menores a 30 años</h3>
-    {filteredUsers.map((user) => <Hello handleClick={(name: string) => handleUser(name)} key={user.email} name={user.name} age={user.age} email={user.email} />)}
-    {/* <HelloClass name={'Salvador'} age={30} email={'salvador@ucreativa.com'} address={'San Jose'} /> */}
-    {/* <Hello name={'Ariel'} age={28} email={'ariel@ucreativa.com'} /> */}
-    {/* <Container users={users}/> */}
-    {/* <Calculator/> */}
-
-    <Wrapper>
-      <Counter />
-      <p>Hola! Este es un ejemplo de un elemento children</p>
-    </Wrapper>
-  </React.Fragment>
+  
+  return <>
+    <Nav/>
+    <Switch>
+      <Route path='/' exact >
+        <Home/>
+      </Route>
+      <Route path='/about' exact >
+        <About/>
+      </Route>
+      <Route path='/todo/:todoSlug'>
+        <MyTodoList/>
+      </Route>
+      <Route>
+        <NotFound/>
+      </Route>
+    </Switch>
+  </>
 }
 
 export default App;
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -10,23 +10,35 @@ interface HelloProps {
 }
 
 export const Hello: React.FC<HelloProps> = ({ name, age, address = 'no address', email, handleClick }) => {
-
-
+	// const isOlder = age >=30 ? 'older' :'young';
+	const isOlder = age>=30;
 	return <Container>
 		<ul onClick={()=> handleClick(name)}>
-			<li>Name: {name}</li>
-			<li>Age: {age}</li>
-			<li>Address: {address}</li>
-			<li> Email: {email}</li>
-			
+			{/* <li className={isOlder}>Name: {name}</li> */}
+			<Item isOlder={isOlder}>Name: {name}</Item>
+			<Item isOlder={isOlder}>Age: {age}</Item>
+			<Item isOlder={isOlder}>Address: {address}</Item>
+			<Item isOlder={isOlder}> Email: {email}</Item>
 		</ul>
-		
-
 	</Container>
 };
 
 const Container = styled.div`
-	width:200px;
+	width:400px;
 	padding: 15px 10px;
-	background: blue ;
+	background:#530b6c ;
+	color: #fff;
+	margin-bottom:15px;
+	border-radius: 6px;
+	ul{
+		list-style: none;
+		padding: 0;
+		margin: 0;
+	}
+`;
+
+const Item = styled.li<{isOlder:boolean}>`
+	padding:0 0 10px 0;
+	text-decoration:${props => props.isOlder ? 'underline': 'none'};
+	font-weight: ${props => props.isOlder ? '800': '300'};
 `;
